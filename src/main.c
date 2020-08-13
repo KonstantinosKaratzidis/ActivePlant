0c52b0a6319544d9f598159aa2cb19d4bae66459
Konstantinos Karatzidis <kostaskarat@hotmail.com>
2020-08-13 11:57:43 +0300

    Fixed buggy state transition in moisture control

--- src/main.c
+++ src/main.c
@@ -30,6 +30,7 @@ void log_status(){}
 #define PERIOD (TICKS_PER_SECOND / 4)
 
 typedef enum {
+	WAIT_INTERVAL,
 	CAN_INSPECT,
 	PUMP_ON, // has opened the pump
 	AFTER_WATER // just after watering, waiting for a certain period
@@ -39,29 +40,39 @@ void moisture_control(){
 	static watering_state_t state = CAN_INSPECT;
 
 	switch(state){
+	case WAIT_INTERVAL:
+	{
+		if(interval_has_passed(INTERVAL_WATER))
+			state = CAN_INSPECT;
+	}
+		break;
 	case CAN_INSPECT:
 	{
-		// TODO: read water level here
+		// TODO: read tank water level here
 		if(moisture_read() < settings_get_moisture_wanted()){
 			pump_open();
 			state = PUMP_ON;
 			interval_set(INTERVAL_PUMP, millis_to_ticks(3000));
 		}
-		break;
 	}
+		break;
 	case PUMP_ON:
 	{
 		if(interval_has_passed(INTERVAL_PUMP)){
 			pump_close();
 			state = AFTER_WATER;
+			interval_set(INTERVAL_WATER_STABILIZE, millis_to_ticks(300 * 1000));
 		}
 	}
+		break;
 	case AFTER_WATER:
 	{
-		if(interval_has_passed(INTERVAL_WATER_STABILIZE))
-			state = CAN_INSPECT;
-		break;
+		if(interval_has_passed(INTERVAL_WATER_STABILIZE)){
+			interval_set(INTERVAL_WATER, millis_to_ticks(settings_get_water_interval() * 1000));
+			state = WAIT_INTERVAL;
+		}
 	}
+		break;
 	}
 }
 
@@ -76,6 +87,9 @@ int main(){
 		if(comm_get_packet(&msg)){
 			process_request(&msg);
 		}
+
+		if(interval_has_passed(INTERVAL_LOG))
+			log_status();
 
 		moisture_control();
 
