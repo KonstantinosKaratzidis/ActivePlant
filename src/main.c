4f97b53b487b9c1ca6c12fb293feef8a0f381e39
Konstantinos Karatzidis <kostaskarat@hotmail.com>
2020-08-13 11:15:05 +0300

    Added callback definitions

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #include "utils.h"
 #include "comm.h"
 
+#define WARN_UNIMPLEMENTED() {dprintf("UNIMPLEMTED %s %d\r\n", __FILE__, __LINE__);} while(0)
+
 void init(){
 	settings_init();
 	test_pin_init();
@@ -30,14 +32,64 @@ void invalid_op_cb(const packet_t *req){
 	comm_send_error(req, COMM_ERR_INVAL_OP);
 }
 
+void ping_cb(const packet_t *req){
+	dprintf("got ping, send pong\r\n");
+	comm_send_packet(req->dst, (1 << COMM_FLAG_RESP), REG_PONG, 0);
+}
+
+void water_level_cb(const packet_t *req){
+	WARN_UNIMPLEMENTED();
+	dprintf("send water level: %d\r\n", 0);
+	comm_send_ok(req, 0);
+}
+
+void moisture_cb(const packet_t *req){
+	WARN_UNIMPLEMENTED();
+	uint16_t moisture_raw = moisture_read_raw();
+	dprintf("moisture: %d\r\n", moisture_raw);
+	comm_send_ok(req, moisture_raw);
+}
+
+void moisture_wanted_cb(const packet_t *req){
+	uint16_t wanted = settings_get_moisture_wanted();
+	dprintf("moisture wanted: %d\r\n", wanted);
+	comm_send_ok(req, wanted);
+}
+
+void water_interval_low_cb(const packet_t *req){
+	uint16_t interval_low = settings_get_water_interval() & 0xffff;
+	dprintf("water interval low: %u\r\n", interval_low);
+	comm_send_ok(req, interval_low);
+}
+
+void water_interval_high_cb(const packet_t *req){
+	uint16_t interval_high = (settings_get_water_interval() >> 16) & 0xffff;
+	dprintf("water interval high: %u\r\n", interval_high);
+	comm_send_ok(req, interval_high);
+}
+
+void log_interval_low_cb(const packet_t *req){
+	uint16_t interval_low = settings_get_log_interval() & 0xffff;
+	dprintf("log interval low: %u\r\n", interval_low);
+	comm_send_ok(req, interval_low);
+}
+
+void log_interval_high_cb(const packet_t *req){
+	uint16_t interval_high = (settings_get_log_interval() >> 16) & 0xffff;
+	dprintf("log interval high: %u\r\n", interval_high);
+	comm_send_ok(req, interval_high);
+}
+
 comm_req_cb comm_callbacks[REG_NUM_REGS] = {
-	[REG_PING] = NULL,
+	[REG_PING] = ping_cb,
 	[REG_PONG] = invalid_op_cb,
-	[REG_WATER_LEVEL] = NULL,
-	[REG_MOISTURE] = NULL,
-	[REG_MOISTURE_WANTED] = NULL,
-	[REG_WATER_INTERVAL] = NULL,
-	[REG_LOG_INTERVAL] = NULL,
+	[REG_WATER_LEVEL] = water_level_cb,
+	[REG_MOISTURE] = moisture_cb,
+	[REG_MOISTURE_WANTED] = moisture_wanted_cb,
+	[REG_WATER_INTERVAL_LOW] = water_interval_low_cb,
+	[REG_WATER_INTERVAL_HIGH] = water_interval_high_cb,
+	[REG_LOG_INTERVAL_LOW] = log_interval_low_cb,
+	[REG_LOG_INTERVAL_HIGH] = log_interval_high_cb,
 };
 
 void process_request(const packet_t *req){
@@ -79,6 +131,7 @@ int main(){
 	init();
 	enable_interrupts();
 	interval_set(INTERVAL_LOG, millis_to_ticks(2000));
+	process_request(NULL);
 
 	char buf[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	for(int i = 0; i < 10; i++){
